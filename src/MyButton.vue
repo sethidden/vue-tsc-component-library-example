23c3e74188cb4c5de8bc829cda8232e426a917a2
Artur Tagisow <artur@tagisow.dev>
2021-10-28 21:58:12 +0200

    remove initial components and add MyButton.vue

---
+++ src/MyButton.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">import { PropType } from '@vue/runtime-core';
+
+defineProps({
+  /* 
+  When someone installs and imports our MyButton.vue component form our library,
+  Volar should offer "calamari" and "tuna" autocompletions for it
+  */
+  seafood: {type: String as PropType<"calamari" | "tuna">, required: true} 
+})
+</script>
+<template>
+<button>
+  I'm a button, my prop is {{ seafood }}
+</button>
+</template>
