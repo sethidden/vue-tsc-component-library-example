fc077c5f47f5f637b4cf594cd63289c038ea67b6
Artur Tagisow <artur@tagisow.dev>
2021-10-28 22:24:40 +0200

    change vite config to lib mode
    https://vitejs.dev/guide/build.html#library-mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,25 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
+// https://vitejs.dev/guide/build.html#library-mode
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.ts'),
+      name: 'MyLib',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
   resolve: {
     alias: {
       '@/': new URL('./src/', import.meta.url).pathname
