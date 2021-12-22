25ecc850165b568e87b7ef11e36928a8fa9a4311
Artur Tagisow <artur@tagisow.dev>
2021-12-22 13:26:51 +0100

    use jest 27 (update deps)

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,7 @@ const config :Config.InitialOptions = {
   globals: {},
   testEnvironment: 'jsdom',
   transform: {
-    "^.+\\.vue$": "vue-jest",
+    "^.+\\.vue$": "@vue/vue3-jest",
     "^.+\\js$": "babel-jest"
   },
   moduleFileExtensions: ['vue', 'js', 'json', 'jsx', 'ts', 'tsx', 'node']
